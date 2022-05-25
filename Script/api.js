7abb3b9cc1f782c0ddb3961d49ee5553c15731e8
JibranH24 <jibranhussain2408@gmail.com>
2022-05-25 12:06:04 +0100

    added design and searchbar works

--- Script/api.js
+++ Script/api.js
@@ -1,56 +1,56 @@
-axios.get("https://api.coinstats.app/public/v1/coins?skip=0")
-  .then(function (response) {
-    return response.data;
-  })
-  .then(function (data) {
+// axios.get("https://api.coinstats.app/public/v1/coins?skip=0")
+//   .then(function (response) {
+//     return response.data;
+//   })
+//   .then(function (data) {
     
-    data.coins.forEach(coin => {
-      const card = document.createElement("div")
-      card.classList.add("card")
-      holder.appendChild(card)
+//     data.coins.forEach(coin => {
+//       const card = document.createElement("div")
+//       card.classList.add("card")
+//       holder.appendChild(card)
 
-      const logo = document.createElement("img")
-      logo.setAttribute("src",coin.icon)
-      logo.classList.add("img")
-      card.appendChild(logo)
+//       const logo = document.createElement("img")
+//       logo.setAttribute("src",coin.icon)
+//       logo.classList.add("img")
+//       card.appendChild(logo)
 
 
-      const coinName = document.createElement("p")
-      coinName.innerHTML=coin.name
-      card.appendChild(coinName)
+//       const coinName = document.createElement("p")
+//       coinName.innerHTML=coin.name
+//       card.appendChild(coinName)
       
-      const coinPrice = document.createElement("p")
-      coinPrice.innerHTML=`$${(coin.price).toFixed(2)}`
-      card.appendChild(coinPrice)
+//       const coinPrice = document.createElement("p")
+//       coinPrice.innerHTML=`$${(coin.price).toFixed(2)}`
+//       card.appendChild(coinPrice)
      
-      // const priceChange1Hour = document.createElement("p")
-      // priceChange1Hour.innerHTML=`Price Change 1 Hour $${coin.priceChange1h}`
-      // card.appendChild(priceChange1Hour)
+//       // const priceChange1Hour = document.createElement("p")
+//       // priceChange1Hour.innerHTML=`Price Change 1 Hour $${coin.priceChange1h}`
+//       // card.appendChild(priceChange1Hour)
       
       
-      const websiteLink =document.createElement("a")
-      var linkText = document.createTextNode("Website");
-      websiteLink.appendChild(linkText);
-      websiteLink.title = "Coin Website";
-      websiteLink.href=coin.websiteUrl
-      card.appendChild(websiteLink);
+//       const websiteLink =document.createElement("a")
+//       var linkText = document.createTextNode("Website");
+//       websiteLink.appendChild(linkText);
+//       websiteLink.title = "Coin Website";
+//       websiteLink.href=coin.websiteUrl
+//       card.appendChild(websiteLink);
 
-      const twitterLink =document.createElement("a")
-      var linkText = document.createTextNode("Twitter");
-      twitterLink.appendChild(linkText);
-      twitterLink.title = "Coin Website";
-      twitterLink.href=coin.twitterUrl
-      card.appendChild(twitterLink);
+//       const twitterLink =document.createElement("a")
+//       var linkText = document.createTextNode("Twitter");
+//       twitterLink.appendChild(linkText);
+//       twitterLink.title = "Coin Website";
+//       twitterLink.href=coin.twitterUrl
+//       card.appendChild(twitterLink);
 
-      const linkDiv = document.createElement("div")
-       linkDiv.classList.add("linkDiv")
-      card.appendChild(linkDiv)
-     })
+//       const linkDiv = document.createElement("div")
+//        linkDiv.classList.add("linkDiv")
+//       card.appendChild(linkDiv)
+//      })
      
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
 
 
 function processForm(form) {
