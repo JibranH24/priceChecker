370f37bc2b197a17aa6e2d675e5d290a500c2297
JibranH24 <jibranhussain2408@gmail.com>
2022-05-27 11:40:37 +0100

    login page Co-authored-by: TVTechEnthusiast01 <TVTechEnthusiast01@users.noreply.github.com> Co-authored-by: hersi21 <hersi21@users.noreply.github.com>

--- public/login-page.js
+++ public/login-page.js
@@ -4,3 +4,23 @@
 // (status 404) the user doesn't exist, so show a popup saying so!
 // (status 403) the user exists but the password is wrong, so show a popup saying so! 
 //res.status
+
+
+
+// let login = async() => {
+//     console.log("login function")
+//     let inputEmail = document.getElementById("inputEmail").value
+//     let inputPassword = document.getElementById("inputPassword").value
+//     axios.post('authenticate',{
+//         email: inputEmail,
+//         password: inputPassword
+//     }) 
+//     .then(function (response) {
+//         console.log(response);
+//         alert("you have logged in" +{first_name})
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+// }
+
